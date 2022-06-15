8e34a68f223f8502513c81ce9b85975d8ecfe690
ionut.marian35 <ionut.marian35@gmail.com>
2022-06-14 22:10:46 -0700

    Adding cards and setting up the Ace

--- BlackJack.py
+++ BlackJack.py
@@ -33,3 +33,14 @@ class Hand:
         self.cards = [] 
         self.value = 0   
         self.aces = 0
+
+    def add_card(self,card):
+        self.cards.append(card)
+        self.value += values[card.rank]
+        #track aces
+        if card.rank=="Ace":
+            self.aces += 1
+    def adjust_for_ace(self):
+        while self.value>21 and self.aces>0:
+            self.value -= 10
+            self.aces -= 1
