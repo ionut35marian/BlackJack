b4a681148160e267da0a86289359049e7bb98883
ionut.marian35 <ionut.marian35@gmail.com>
2022-06-14 22:17:21 -0700

    Game_on

--- BlackJack.py
+++ BlackJack.py
@@ -128,3 +128,46 @@ def dealer_wins(player,dealer,chips):
     chips.lose_bet()
 def push(player,dealer):
     print("Dealer and player tie! Push")
+
+while True:
+    print("Welcome to BlackJack! Get as close to 21 as you can without going over!")
+    deck=Deck()
+    deck.shuffle()
+
+    player_hand=Hand()
+    player_hand.add_card(deck.deal())
+    player_hand.add_card(deck.deal())
+
+    dealer_hand=Hand()
+    dealer_hand.add_card(deck.deal())
+    dealer_hand.add_card(deck.deal())
+    
+    player_chips=Chips()
+    take_bet(player_chips)
+    show_some(player_hand,dealer_hand)
+    while playing:
+        hit_or_stand(deck,player_hand)
+        show_some(player_hand,dealer_hand)
+        if player_hand.value>21:
+            player_busts(player_hand,dealer_hand,player_chips)
+            break
+    
+    if player_hand.value<=21:
+        while dealer_hand.value<17:
+            hit(deck,dealer_hand)
+        show_all(player_hand,dealer_hand)
+        if dealer_hand.value>21:
+            dealer_busts(player_hand,dealer_hand,player_chips)
+        elif dealer_hand.value>player_hand.value:
+            dealer_wins(player_hand,dealer_hand,player_chips)
+        elif dealer_hand.value<player_hand.value:
+            player_wins(player_hand,dealer_hand,player_chips)
+        else:
+            push(player_hand,dealer_hand)
+    new_game=input("Would you like to play another band? Enter 'y' or 'n'")
+    if new_game[0].lower()=='y':
+        playing=True
+        continue
+    else:
+        print("Thanks for playing")
+        break
