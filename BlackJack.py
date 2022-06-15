5fe1a643860b6b6172523c1d69f9e43bf7163b3c
ionut.marian35 <ionut.marian35@gmail.com>
2022-06-14 22:12:42 -0700

    Actions

--- BlackJack.py
+++ BlackJack.py
@@ -76,3 +76,8 @@ def take_bet(chips):
                 print("Sorry, you do not have enough chips! You have: {}".format(chips.total))
             else:
                 break
+
+def hit(deck,hand):
+    single_card=deck.deal()
+    hand.add_card(single_card)
+    hand.adjust_for_ace()
